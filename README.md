38d93d63fc3cd145dab038086d148f57c4c44fe4
Berat <berat230@gmail.com>
2025-09-10 00:55:28 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# trainer-hours-service
+
---
+++ src/main/java/com/epam/gym/trainer_hours_service/TrainerHoursServiceApplication.java
@@ -0,0 +1,13 @@
+package com.epam.gym.trainer_hours_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainerHoursServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainerHoursServiceApplication.class, args);
+	}
+
+}
