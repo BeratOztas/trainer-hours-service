086812057dcfdb5d3a0261d53dd172964220bfb7
Berat <berat230@gmail.com>
2025-09-21 16:31:50 +0300

    feat: TrainerWorkloadService - Implemented upsert logic and refactored DTOs
    Refactored the TrainerWorkloadResponse DTO to align with the new upsert logic, ensuring the response accurately reflects the updated workload summary.
    
    Removed the redundant WorkloadMapConverter class, as its functionality is now handled more efficiently within the TrainerWorkloadServiceImpl.
    
    Ensured that totalTrainingMinutes is correctly incremented or decremented based on the ActionType (ADD/DELETE) of the incoming Kafka events.
    
    Added comprehensive unit tests to cover all scenarios, including upsert logic for both new and existing trainers, as well as GET operations.

--- src/main/java/com/epam/gym/trainer_hours_service/api/dto/response/TrainerWorkloadResponse.java
+++ src/main/java/com/epam/gym/trainer_hours_service/api/dto/response/TrainerWorkloadResponse.java
@@ -1,42 +1,26 @@
 package com.epam.gym.trainer_hours_service.api.dto.response;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.epam.gym.trainer_hours_service.db.entity.TrainerWorkload;
-
+import com.epam.gym.trainer_hours_service.db.entity.TrainerWorkload.YearlySummary;
 
 public record TrainerWorkloadResponse(
 		String trainerUsername,
 		String trainerFirstName,
 		String trainerLastName,
-		boolean isActive,
+		Boolean isActive,
 		List<YearlySummary> yearlySummaries
 		) {
 	
 	public static TrainerWorkloadResponse fromEntity(TrainerWorkload trainerWorkload) {
-		List<YearlySummary> yearlySummaries = trainerWorkload.getYearlySummary().entrySet().stream()
-				.map(yearEntry -> {
-					List<MonthlySummary> monthlySummaries = yearEntry.getValue().entrySet().stream()
-							.map(monthEntry -> new MonthlySummary(monthEntry.getKey(), monthEntry.getValue()))
-							.collect(Collectors.toList());
-					return new YearlySummary(yearEntry.getKey(), monthlySummaries);
-				}).collect(Collectors.toList());
-		
+	
 		return new TrainerWorkloadResponse(
 						trainerWorkload.getTrainerUsername(),
 						trainerWorkload.getTrainerFirstName(),
 						trainerWorkload.getTrainerLastName(),
-						trainerWorkload.isActive(),
-						yearlySummaries);
+						trainerWorkload.getIsActive(),
+						trainerWorkload.getYearlySummary());
 	}
-	public record YearlySummary(
-			int year,
-			List<MonthlySummary> monthlySummary
-	) {}
 	
-	public record MonthlySummary(
-			int month,
-			int totalTrainingMinutes
-	) {}
 }
